10c43ea57e4049fe2cd93e6e3bbefa60f796b01a
jaloliddin_ruzikulov <jaloliddin1ruzikulov@gmail.com>
2022-11-22 16:11:40 +0500

    New bug fixes

--- apps/urls.py
+++ apps/urls.py
@@ -1,18 +1,16 @@
 from django.urls import path
-from .views import appsView,addRatsiya,userManager,RatDetailView,TadbirView,TadbirViews,RatListView,Adds,eventClose,omborListView,some_view,printerView
+from .views import apps_view, add_ratsiya, user_manager, RatDetailView, tadbir_view, TadbirViews, RatListView,  \
+    event_close, OmborListView,  printer_view
+
 urlpatterns = [
-    path('add/',addRatsiya, name ='add'),
-    path('main/',appsView,name='main'),
-    path('events/<int:tadbirid>/',TadbirView,name='detialevent'),      
-    path('events/',TadbirViews.as_view(),name='events'),  
-    path('lists/',RatListView.as_view(),name='lists'),    
-    path('ombor/',omborListView.as_view(),name='ombor'),        
-    path('some_view/',some_view,name='some_view'),    
-    path('',userManager, name='logins'),
+    path('add/', add_ratsiya, name='add'),
+    path('main/', apps_view, name='main'),
+    path('events/<int:tadbirid>/', tadbir_view, name='detialevent'),
+    path('events/', TadbirViews.as_view(), name='events'),
+    path('lists/', RatListView.as_view(), name='lists'),
+    path('ombor/', OmborListView.as_view(), name='ombor'),
+    path('', user_manager, name='logins'),
     path('detial/<int:pk>/', RatDetailView.as_view(), name='detial'),
-    path('events/<int:tadbirid>/close',eventClose, name='CEvent'),
-    path('events/<int:tadbirid>/print',printerView, name='printer'),
+    path('events/<int:tadbirid>/close', event_close, name='CEvent'),
+    path('events/<int:tadbirid>/print', printer_view, name='printer'),
 ]
-
-
-
--- apps/views.py
+++ apps/views.py
@@ -1,217 +1,123 @@
-from multiprocessing import context
-from re import search
-from urllib import request
-from django.shortcuts import render, get_object_or_404
-from .models import ratsiyaModel,tadbirModel,Enrollment
 from django.shortcuts import redirect
-from django.views.generic import CreateView,DetailView,ListView,TemplateView
-from django.views.decorators.http import require_http_methods
-import io
-from django.http import FileResponse
-from reportlab.pdfgen import canvas
+from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
-def ratsiyaPrint(request,datalist):
-    from reportlab.lib import colors
-    from reportlab.lib.pagesizes import letter
-    from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
-    from reportlab.lib.pagesizes import letter, landscape
+from .appm.pdfgen import pdf_printer
+from .models import ratsiyaModel, tadbirModel, Enrollment
 
-    buffer = io.BytesIO()
-    doc = SimpleDocTemplate("simple_table.pdf", pagesize=landscape(letter))
-# container for the 'Flowable' objects
-    elements = []
- # T/r n0 Seriya nomer Ism Familiya Soxa xizmat Tel nomer Qaytarildi
-    data= [['T/r ', 'N_', 'Seriya nomer', 'Ism Familiya', 'Soxa xizmat','Tel nomer','Imzo','Qaytarildi']]
-    tr=0
-    for obyekt in datalist:
-        datas=[]
-        tr+=1
-        datas.append(tr)
-        datas.append(obyekt[0].rcode)
-        datas.append(obyekt[0].qr_code)
-        data.append(datas)
 
-    t=Table(data,100, 50)
-    t.setStyle(TableStyle([('ALIGN',(1,1),(-2,-2),'RIGHT'),
-    ('VALIGN',(0,0),(0,-1),'TOP'),
-    ('TEXTCOLOR',(0,0),(-1,-1),colors.black),
-    ('ALIGN',(0,0),(-1,-1),'CENTER'),
-    ('VALIGN',(0,0),(-1,-1),'MIDDLE'),
-    ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
-    ('BOX', (0,0), (-1,-1), 0.25, colors.black),
-    ]))
-    t._argW[0],t._argW[1],t._argW[2],t._argW[3],t._argW[4]=30,30,100,150,150
-    elements.append(t)
-# write the document to disk
-    doc.build(elements)    
-    return FileResponse(open('simple_table.pdf','rb'), as_attachment=False, filename='hello.pdf')
-
-def boshqaPrint(request,datalist):
-    from reportlab.lib import colors
-    from reportlab.lib.pagesizes import letter
-    from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
-    from reportlab.lib.pagesizes import letter, landscape
-
-    buffer = io.BytesIO()
-    doc = SimpleDocTemplate("simple_table.pdf", pagesize=letter)
-# container for the 'Flowable' objects
-    elements = []
- # T/r n0 Seriya nomer Ism Familiya Soxa xizmat Tel nomer Qaytarildi
-    data= [['T/r ','Katalog', 'Model', 'QR Code']]
-    tr=0
-    for obyekt in datalist:
-        datas=[]
-        tr+=1
-        datas.append(tr)
-        datas.append(obyekt[0].katalog)
-        datas.append(obyekt[0].model)        
-        datas.append(obyekt[0].qr_code)
-        data.append(datas)
-
-    t=Table(data,100, 50)
-    t.setStyle(TableStyle([('ALIGN',(1,1),(-2,-2),'RIGHT'),
-    ('VALIGN',(0,0),(0,-1),'TOP'),
-    ('TEXTCOLOR',(0,0),(-1,-1),colors.black),
-    ('ALIGN',(0,0),(-1,-1),'CENTER'),
-    ('VALIGN',(0,0),(-1,-1),'MIDDLE'),
-    ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
-    ('BOX', (0,0), (-1,-1), 0.25, colors.black),
-    ]))
-    t._argW[0]= 50
-    elements.append(t)
-# write the document to disk
-    doc.build(elements)    
-    return FileResponse(open('simple_table.pdf','rb'), as_attachment=False, filename='hello.pdf')
-
-def printerView(request,tadbirid):
-    if (tadbirModel.objects.get(id=tadbirid).nametadbir) in ['Prezident tashrif','Yangi yil','Festivallar','Futbol','DTM Test','Bazaga qaytarish']:
-          datalist = []
-          enrolments = Enrollment.objects.filter(tadbirModel_id=tadbirid)
-          for datas in enrolments:
-              datalist.append(ratsiyaModel.objects.filter(id=datas.ratsiyaModel_id))
-          return ratsiyaPrint(request,datalist)
+def printer_view(request, tadbirid):
+    if tadbirModel.objects.get(id=tadbirid).nametadbir in ['Prezident tashrif', 'Yangi yil', 'Festivallar', 'Futbol',
+                                                           'DTM Test', 'Bazaga qaytarish']:
+        datalist = []
+        enrolments = Enrollment.objects.filter(tadbirModel_id=tadbirid)
+        for datas in enrolments:
+            datalist.append(ratsiyaModel.objects.filter(id=datas.ratsiyaModel_id))
+        return pdf_printer(request, datalist, idevents=tadbirid, ratsiya=True)
     else:
-          datalist = []
-          enrolments = Enrollment.objects.filter(tadbirModel_id=tadbirid)
-          for datas in enrolments:
-              datalist.append(ratsiyaModel.objects.filter(id=datas.ratsiyaModel_id))
-          return boshqaPrint(request,datalist)    
+        datalist = []
+        enrolments = Enrollment.objects.filter(tadbirModel_id=tadbirid)
+        for datas in enrolments:
+            datalist.append(ratsiyaModel.objects.filter(id=datas.ratsiyaModel_id))
+        return pdf_printer(request, datalist, idevents=tadbirid, ratsiya=False)
 
 
-def some_view(request):
-    from reportlab.lib import colors
-    from reportlab.lib.pagesizes import letter
-    from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
-    from reportlab.lib.pagesizes import letter, landscape
-
-    buffer = io.BytesIO()
-    doc = SimpleDocTemplate("simple_table.pdf", pagesize=landscape(letter))
-# container for the 'Flowable' objects
-    elements = []
- # T/r n0 Seriya nomer Ism Familiya Soxa xizmat Tel nomer Qaytarildi
-    data= [['T/r ', 'N_', 'Seriya nomer', 'Ism Familiya', 'Soxa xizmat','Tel nomer','Imzo','Qaytarildi'],
-    ['30', '31', '32', '', '34', '23', '24']]
-    t=Table(data,100, 50)
-    t.setStyle(TableStyle([('ALIGN',(1,1),(-2,-2),'RIGHT'),
-    ('VALIGN',(0,0),(0,-1),'TOP'),
-    ('TEXTCOLOR',(0,0),(-1,-1),colors.black),
-    ('ALIGN',(0,0),(-1,-1),'CENTER'),
-    ('VALIGN',(0,0),(-1,-1),'MIDDLE'),
-    ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
-    ('BOX', (0,0), (-1,-1), 0.25, colors.black),
-    ]))
-    t._argW[0],t._argW[1],t._argW[2],t._argW[3],t._argW[4]=30,30,100,150,150
-    elements.append(t)
-# write the document to disk
-    doc.build(elements)    
-    return FileResponse(open('simple_table.pdf','rb'), as_attachment=False, filename='hello.pdf')
-    
-def eventClose(request,tadbirid):
+def event_close(request, tadbirid):
     tadbirModel.objects.filter(id=tadbirid).update(closeEvent=True)
-    return redirect('/events/'+str(tadbirid))   
+    return redirect('/events/' + str(tadbirid))
 
-def addRatsiya(request):
-    context={}
-    context['group'] =  str(request.user.groups.all()[0])
-    if request.POST and request.POST['katalog']=='Ratsiya':
-        countit = int(request.POST['counts'])+1
-        for i in range(1,countit):
-            if not ratsiyaModel.objects.filter(qr_code=str(request.POST['field'+str(i)])):
-                ratsiyaModel.objects.get_or_create(katalog=str(request.POST['katalog']),model=str(request.POST['model']),qr_code=str(request.POST['field'+str(i)]),rcode=str(request.POST['quantity'+str(i)]))
-        return redirect('/detial/'+str((ratsiyaModel.objects.last()).id))
+
+def add_ratsiya(request):
+    context = dict()
+    context['group'] = str(request.user.groups.all()[0])
+    if request.POST and request.POST['katalog'] == 'Ratsiya':
+        countit = int(request.POST['counts']) + 1
+        for i in range(1, countit):
+            if not ratsiyaModel.objects.filter(qr_code=str(request.POST['field' + str(i)])):
+                ratsiyaModel.objects.get_or_create(katalog=str(request.POST['katalog']),
+                                                   model=str(request.POST['model']),
+                                                   qr_code=str(request.POST['field' + str(i)]),
+                                                   rcode=str(request.POST['quantity' + str(i)]))
+        return redirect('/detial/' + str((ratsiyaModel.objects.last()).id))
 
     if request.POST:
-        countit = int(request.POST['counts'])+1
-        for i in range(1,countit):
-            if not ratsiyaModel.objects.filter(qr_code=str(request.POST['field'+str(i)])):
-                ratsiyaModel.objects.get_or_create(katalog=str(request.POST['katalog']),model=str(request.POST['model']),qr_code=str(request.POST['field'+str(i)]))
-        return redirect('/detial/'+str((ratsiyaModel.objects.last()).id))
-    return render(request,'add.html',context)
+        countit = int(request.POST['counts']) + 1
+        for i in range(1, countit):
+            if not ratsiyaModel.objects.filter(qr_code=str(request.POST['field' + str(i)])):
+                ratsiyaModel.objects.get_or_create(katalog=str(request.POST['katalog']),
+                                                   model=str(request.POST['model']),
+                                                   qr_code=str(request.POST['field' + str(i)]))
+        return redirect('/detial/' + str((ratsiyaModel.objects.last()).id))
+    return render(request, 'add.html', context)
+
 
 class RatDetailView(DetailView):
     model = ratsiyaModel
-    template_name = 'detail.html'    
-##################################
+    template_name = 'detail.html'
+
+
 class TadbirViews(ListView):
     model = tadbirModel
-    template_name = 'events.html'    
-  
+    template_name = 'events.html'
+
+
 class EventsListView(ListView):
     model = ratsiyaModel
-    template_name = 'lists.html'    
+    template_name = 'lists.html'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         listoff = tadbirModel.objects.all()
-        context['tadbirlid'] = list(listoff.values( 'id','nametadbir'))   
-        return context    
-###############################
+        context['tadbirlid'] = list(listoff.values('id', 'nametadbir'))
+        return context
+
 
 class RatListView(ListView):
     model = ratsiyaModel
-    template_name = 'lists.html'    
+    template_name = 'lists.html'
     context_object_name = 'profiles'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         listoff = tadbirModel.objects.all()
-        context['tadbirlid'] = list(listoff.values( 'id','nametadbir'))   
-        return context    
+        context['tadbirlid'] = list(listoff.values('id', 'nametadbir'))
+        return context
 
-class omborListView(ListView):
+
+class OmborListView(ListView):
     model = ratsiyaModel
-    template_name = 'ombor.html'    
+    template_name = 'ombor.html'
     context_object_name = 'profiles'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         listoff = tadbirModel.objects.all()
-        context['tadbirlid'] = list(listoff.values( 'id','nametadbir'))   
-        return context    
+        context['tadbirlid'] = list(listoff.values('id', 'nametadbir'))
+        return context
 
 
-def userManager(request):
+def user_manager(request):
     if request.user.is_authenticated:
         return redirect('main')
     else:
-        return redirect('login')      
-   
-def appsView(request):
-    context={}
-    context['group'] =  str(request.user.groups.all()[0])
+        return redirect('login')
+
+
+def apps_view(request):
+    context = dict()
+    context['group'] = str(request.user.groups.all()[0])
     if 'browser' in request.POST:
         searchbox = request.POST['browser']
-        tadbirModel.objects.create(nametadbir = searchbox ,authuser= request.user)
-        return redirect('/events/'+str((tadbirModel.objects.last()).id))
+        tadbirModel.objects.create(nametadbir=searchbox, authuser=request.user)
+        return redirect('/events/' + str((tadbirModel.objects.last()).id))
     elif 'sabab' in request.POST:
         searchbox = request.POST['sabab']
-        tadbirModel.objects.create(nametadbir = searchbox ,authuser= request.user)
-        return redirect('/events/'+str((tadbirModel.objects.last()).id))
-    return render(request,'index.html',context)
+        tadbirModel.objects.create(nametadbir=searchbox, authuser=request.user)
+        return redirect('/events/' + str((tadbirModel.objects.last()).id))
+    return render(request, 'index.html', context)
 
 
-
-
-def TadbirView(request,tadbirid):
+def tadbir_view(request, tadbirid):
     context = {}
     datalist = []
     enrolments = Enrollment.objects.filter(tadbirModel_id=tadbirid)
@@ -221,31 +127,27 @@ def TadbirView(request,tadbirid):
     tadbir = tadbirModel.objects.get(id=tadbirid)
     context['tadbir'] = tadbir
     if tadbir.nametadbir == 'Bazaga qaytarish':
-            if 'sabab' in request.POST and tadbir.closeEvent ==False:
-                searchbox = (request.POST['sabab']).strip()
-                rMid = ratsiyaModel.objects.filter(qr_code=searchbox)
-                if rMid and rMid.values('lasteventid')[0]['lasteventid']!=0:
-                    lastevents = ratsiyaModel.objects.get(qr_code=str(searchbox))
-                    lastevents.lasteventid = 0
-                    lastevents.save()
-                    rMid=rMid.values('id')[0]['id']
-                    s1=Enrollment(tadbirModel_id = tadbirid , ratsiyaModel_id = rMid )
-                    s1.save()
-                    return redirect('/events/'+str(tadbirid)+'/')
-        
-    if 'sabab' in request.POST and tadbir.closeEvent ==False and tadbir.nametadbir != 'Bazaga qaytarish':
+        if 'sabab' in request.POST and not tadbir.closeEvent:
+            searchbox = (request.POST['sabab']).strip()
+            rmid = ratsiyaModel.objects.filter(qr_code=searchbox)
+            if rmid and rmid.values('lasteventid')[0]['lasteventid'] != 0:
+                lastevents = ratsiyaModel.objects.get(qr_code=str(searchbox))
+                lastevents.lasteventid = 0
+                lastevents.save()
+                rmid = rmid.values('id')[0]['id']
+                s1 = Enrollment(tadbirModel_id=tadbirid, ratsiyaModel_id=rmid)
+                s1.save()
+                return redirect('/events/' + str(tadbirid) + '/')
+
+    if 'sabab' in request.POST and not tadbir.closeEvent and tadbir.nametadbir != 'Bazaga qaytarish':
         searchbox = (request.POST['sabab']).strip()
-        rMid = ratsiyaModel.objects.filter(qr_code=searchbox)
-        if rMid and rMid.values('lasteventid')[0]['lasteventid']!=tadbirid:
+        rmid = ratsiyaModel.objects.filter(qr_code=searchbox)
+        if rmid and rmid.values('lasteventid')[0]['lasteventid'] != tadbirid:
             lastevents = ratsiyaModel.objects.get(qr_code=str(searchbox))
             lastevents.lasteventid = tadbirid
             lastevents.save()
-            rMid=rMid.values('id')[0]['id']
-            s1=Enrollment(tadbirModel_id = tadbirid , ratsiyaModel_id = rMid )
+            rmid = rmid.values('id')[0]['id']
+            s1 = Enrollment(tadbirModel_id=tadbirid, ratsiyaModel_id=rmid)
             s1.save()
-            return redirect('/events/'+str(tadbirid)+'/')
-    return render(request,'details.html',context)
-def Adds(request):
-    return render(request,'adds.html')    
-
-   
+            return redirect('/events/' + str(tadbirid) + '/')
+    return render(request, 'details.html', context)
